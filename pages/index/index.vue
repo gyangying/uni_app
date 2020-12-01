bd9b7254d1cc8c9feb8cc9c3a602b38760bc0111
guojiaxin <gyangying@163.com>
2020-12-01 10:22:07 +0800

    第一次提交

---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<template v-for="(item,index) in list">
+			<view>{{item.create_time}}</view>
+			<view class="">
+				
+			</view>
+		</template>
+	</view>
+</template>
+
+<script>
+	data () {
+		return {
+			list: [], // 消息列表
+			page: 1,
+			
+		}
+	}
+</script>
+
+<style>
+	
+	
+</style>
